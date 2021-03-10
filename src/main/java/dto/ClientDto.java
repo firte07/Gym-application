af7a976b72e86aa50f56687b7b850c398a5ec00c
firte07 <45260144+firte07@users.noreply.github.com>
2021-03-10 09:50:53 +0200

    Create ClientDto.java

---
+++ src/main/java/dto/ClientDto.java
@@ -0,0 +1,13 @@
+package dto;
+
+public class ClientDto {
+    private String idClient;
+
+    public String getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(String idClient) {
+        this.idClient = idClient;
+    }
+}
