0ed3c529533535e128428ed8b5da10c42ceaf08d
firte07 <45260144+firte07@users.noreply.github.com>
2021-03-10 09:53:44 +0200

    Create FrameDesign.java

---
+++ src/main/java/utils/FrameDesign.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.awt.*;
+
+public class FrameDesign {
+    public static final Color NAVY_BLUE = new Color(42, 50, 75);
+    public static final Color GREY = new Color(208, 204, 208);
+    public static final Color CREAM = new Color(251, 209, 162);
+    public static final Color LIGHT_BLUE = new Color(77, 114, 152);
+    public static final Color DARK_PINK = new Color(180, 101, 111);
+
+    public static final Font CALIBRI_BOLD_14 = new Font("Calibri", Font.BOLD, 14);
+    public static final Font CALIBRI_BOLD_16 = new Font("Calibri", Font.BOLD, 16);
+    public static final Font CALIBRI_SIMPLE_14 = new Font("Calibri", Font.PLAIN, 14);
+    public static final Font CALIBRI_ITALIC_14 = new Font("Calibri", Font.ITALIC, 14);
+
+}
