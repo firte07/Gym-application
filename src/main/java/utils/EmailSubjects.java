6c7f3a344aaf2b1a02a0d4d7132fe9bd27a3ba0b
firte07 <45260144+firte07@users.noreply.github.com>
2021-03-10 09:53:30 +0200

    Create EmailSubjects.java

---
+++ src/main/java/utils/EmailSubjects.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class EmailSubjects {
+    public static final String REGISTER_EMAIL_SUBJEECT = "FITNESS APP: Thank you for your registration - Do not reply";
+    public static final String CREDENTIALS_CHANGED_EMAIL_SUBJEECT = "FITNESS APP: Your credentials have just been changed";
+}
