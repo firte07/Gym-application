91403da1656546a7d56aef22b1ae49e02fa74dba
firte07 <45260144+firte07@users.noreply.github.com>
2021-03-10 09:55:14 +0200

    Create MembershipDto.java

---
+++ src/main/java/dto/MembershipDto.java
@@ -0,0 +1,46 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+public class MembershipDto {
+
+    private String idMembership;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime expirationDate;
+
+    private String membershipType;
+
+    public String getIdMembership() {
+        return idMembership;
+    }
+
+    public void setIdMembership(String idMembership) {
+        this.idMembership = idMembership;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public void setMembershipType(String membershipType) {
+        this.membershipType = membershipType;
+    }
+}
