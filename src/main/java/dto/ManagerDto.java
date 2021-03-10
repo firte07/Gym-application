0e2002a9c1d8f4e79bab8fe1dc1adc61c85ac6ef
firte07 <45260144+firte07@users.noreply.github.com>
2021-03-10 09:50:56 +0200

    Create ManagerDto.java

---
+++ src/main/java/dto/ManagerDto.java
@@ -0,0 +1,35 @@
+package dto;
+
+public class ManagerDto {
+
+    private String name;
+
+    private String phoneNr;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
