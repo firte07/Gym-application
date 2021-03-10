32704b814d837f0fef26d8ae46d961a82b07d1c7
firte07 <45260144+firte07@users.noreply.github.com>
2021-03-10 09:55:22 +0200

    Create TrainerDto.java

---
+++ src/main/java/dto/TrainerDto.java
@@ -0,0 +1,25 @@
+package dto;
+
+public class TrainerDto {
+
+    private String nameTrainer;
+
+    private String fitnessClassName;
+
+
+    public String getNameTrainer() {
+        return nameTrainer;
+    }
+
+    public void setNameTrainer(String nameTrainer) {
+        this.nameTrainer = nameTrainer;
+    }
+
+    public String getFitnessClassName() {
+        return fitnessClassName;
+    }
+
+    public void setFitnessClassName(String fitnessClassName) {
+        this.fitnessClassName = fitnessClassName;
+    }
+}
